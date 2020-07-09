c70dff8b8048dd8ffc201dca0d5fdc78bea5ed32
Yahya <yahyatinani@gmail.com>
2020-07-09 15:16:53 +0100

    Refactor contestFactory using map function

--- src/Contest.kt
+++ src/Contest.kt
@@ -1,20 +1,9 @@
 data class Contestant<T : Pet>(val pet: T, val score: Int)
 
-fun <T : Pet> addContestant(contest: Contest<T>, contestant: Contestant<out T>): Contest<T> {
-    return contest.copy(scores = contest.scores.plus(contestant.pet to contestant.score))
+fun <T : Pet> contestFactory(contestants: List<Contestant<out T>>, vet: Vet<in T>): Contest<T> {
+    val map = contestants.map { it.pet to it.score }.toMap()
+    return Contest(map, vet)
 }
-
-private tailrec fun <T : Pet> makeContestIter(
-    contestants: List<Contestant<out T>>,
-    contest: Contest<T>
-): Contest<T> {
-    if (contestants.isEmpty()) return contest
-
-    return makeContestIter(rest(contestants), addContestant(contest, contestants.first()))
-}
-
-fun <T : Pet> contestFactory(contestants: List<Contestant<out T>>, vet: Vet<in T>): Contest<T> =
-    makeContestIter(rest(contestants), Contest(vet = vet))
 
 data class Contest<T : Pet>(val scores: Map<T, Int> = mapOf(), val vet: Vet<in T>) {
     fun getWinners(): Set<T> {
